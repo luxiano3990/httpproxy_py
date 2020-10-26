46878c3e271a1b89466bb0ee511cd5895e5d7073
luxiano3990 <marchese.luciano@gmail.com>
2020-10-26 09:34:25 +0100

    Update request_client_post.py

--- request_client_post.py
+++ request_client_post.py
@@ -5,8 +5,7 @@ import jwt
 
 from datetime import date
 
-#iat=date.today().strftime("%d/%m/%Y")
-#Jti='MyID-001'
+dat=date.today().strftime("%d/%m/%Y")
 
 # define private secret
 secret=('A9ddbcaba8c0ac1a0a812dc0c2f08514b23f2db0a68343cb8199ebb38a6d91e4ebfb378e22ad39c2d01 d0b4ec9c34aa91056862ddace3fbbd6852ee60c36acbf')
